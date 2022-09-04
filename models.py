8ab015004c2bd45b5abb2c01a176377ce418e32a
Thiago Sarmento <thiago.sarmento@itec.ufpa.br>
2022-09-04 01:55:44 -0300

    Add classes attributes (Tiefling, Half-Orc, Half-Elf, Human, Halfling, Dwarf)

---
+++ models.py
@@ -0,0 +1,233 @@
+shortsword = {
+            "name": "shortsword",
+            "dices": [6],
+            "base damage": 2,
+            "bonus damage": 0,
+            "damage type": "piercing",
+            "weight": 0,
+            "attack range": 1.5
+}
+
+bare_hands = {
+            "name": "bare hands",
+            "dices": [1],
+            "base damage": 0,
+            "bonus damage": 0,
+            "damage type": "bludgeoning",
+            "weight": 0,
+            "attack range": 1.5
+            }
+
+greatsword = {
+            "name": "greatsword",
+            "dices": [6,6],
+            "base damage": 0,
+            "bonus damage": 0,
+            "damage type": "slashing",
+            "weight": 4,
+            "attack range": 1.5
+            }
+
+skeleton = {
+    "name": "Skeleton",
+    "xp": 50,
+    "hp": 13,
+    "armor class": 13,
+    "speed": [30,30],
+    "weapon": shortsword
+}
+
+test_char = {
+    "name": "Stranger",
+    "level": 1,
+    "xp": 0,
+    "hp": 20,
+    "armor class": 15,
+    "speed": [30,30],
+    "weapon": greatsword
+}
+
+ABILITIES_LIST = {
+    "Strength": 0,
+    "Dexterity": 0,
+    "Constitution": 0,
+    "Intelligence": 0,
+    "Wisdom": 0,
+    "Charisma": 0
+}
+
+ability_test = {
+    "Strength": 0,
+    "Dexterity": 0,
+    "Constitution": 0,
+    "Intelligence": 0,
+    "Wisdom": 0,
+    "Charisma": 0
+}
+
+skill_str = {
+    "ability": "Strenght",
+    "bonus": 0
+}
+
+skill_dex = {
+    "ability": "Dexterity",
+    "bonus": 0
+}
+
+skill_con = {
+    "ability": "Constitution",
+    "bonus": 0
+}
+
+skill_int = {
+    "ability": "Intelligence",
+    "bonus": 0
+}
+
+skill_wis = {
+    "ability": "Wisdom",
+    "bonus": 0
+}
+
+skill_cha = {
+    "ability": "Charisma",
+    "bonus": 0
+}
+
+skills_test = {
+    "Acrobatics": skill_dex,
+    "Animal Handling": skill_wis,
+    "Arcana": skill_int,
+    "Athletics": skill_str,
+    "Deception": skill_cha,
+    "History": skill_int,
+    "Insight": skill_wis,
+    "Intimidation": skill_cha,
+    "Investigation": skill_int,
+    "Medicine": skill_wis,
+    "Nature": skill_int,
+    "Perception": skill_wis,
+    "Performance": skill_cha,
+    "Persuasion": skill_cha,
+    "Religion": skill_int,
+    "Sleight of Hands": skill_dex,
+    "Stealth": skill_dex,
+    "Survival": skill_wis
+}
+
+test_char_complete = {
+    "name": "Stranger",
+    "level": 1,
+    "xp": 0,
+    "race": "Human",
+    "class": "Fighter",
+    "abilities": ability_test,
+    "skills": skills_test,
+    "hp": 0,
+    "armor class": 0,
+    "iniciative": 0,
+    "speed": 0,
+    "weapon": greatsword
+}
+
+############ Dwarf race info ##########
+DWARF_PROFICIENCY = ["Battleaxe","Handaxe","Light hammer","Warhammer"]
+DWARF_LANGUAGE = ["Dwarvish"]
+DWARF_HILL_SUBRACE_ABILITY_BONUS={
+        "Constitution": 2,
+        "Wisdom":       1
+}
+DWARF_MONTAIN_SUBRACE_PROFICIENCY = ["Light armor","Medium armor"]
+DWARF_MONTAIN_SUBRACE_ABILITY_BONUS={
+        "Strength":     2,
+        "Constitution": 2,
+}
+########### Elf race info
+ELF_PROFICIENCY = ["Perception"]
+ELF_LANGUAGE = ["Elvish"]
+ELF_HIGH_SUBRACE_PROFICIENCY = ["Longsword","Shortsword","Shortbow","Longbow"]
+ELF_HIGH_SUBRACE_ABILITY_BONUS = {
+    "Dexterity":    2,
+    "Intelligence": 1
+}
+ELF_WOOD_SUBRACE_PROFICIENCY = ["Longsword","Shortsword","Shortbow","Longbow"]
+ELF_WOOD_SUBRACE_ABILITY_BONUS = {
+    "Dexterity":    2,
+    "Wisdom":       1
+}
+ELF_DROW_SUBRACE_PROFICIENCY = ["Rapiers","Shortswords","Hand crossbows"]
+ELF_DROW_SUBRACE_ABILITY_BONUS = {
+    "Dexterity":    2,
+    "Charisma":     1
+}
+########### Halfling race info
+HALFLING_LANGUAGE = ["Halfling"]
+HALFLING_LIGHTFOOT_SUBRACE_ABILITY_BONUS={
+    "Dexterity":    2,
+    "Charisma":     1
+}
+HALFLING_STOUT_SUBRACE_ABILITY_BONUS={
+    "Dexterity":    2,
+    "Constitution": 1
+}
+########### Human race info
+HUMAN_RACE_ABILITY_BONUS = {
+    "Strength":     1,
+    "Dexterity":    1,
+    "Constitution": 1,
+    "Intelligence": 1,
+    "Wisdom":       1,
+    "Charisma":     1
+}
+########### Dragonborn race info
+DRAGONBORN_LANGUAGE = ["Draconic"]
+DRAGONBORN_RACE_ABILITY_BONUS = {
+    "Strength":     2,
+    "Charisma":     1
+}
+########### Gnome race info
+GNOME_LANGUAGE = ["Gnomish"]
+GNOME_FOREST_SUBRACE_ABILITY_BONUS = {
+    "Dexterity": 1,
+    "Intelligence": 2
+}
+GNOME_ROCK_SUBRACE_ABILITY_BONUS = {
+    "Constitution": 1,
+    "Intelligence": 2
+}
+########### Half Elf info
+HALF_ELF_RACE_ABILITY_BONUS = {
+    "Charisma":     2
+}
+########### Half Orc info
+HALF_ORC_RACE_ABILITY_BONUS = {
+    "Strength":     2,
+    "Constitution": 1
+}
+########### Tiefling
+TIEFLING_RACE_ABILITY_BONUS = {
+    "Intelligence": 1,
+    "Charisma":     2
+}
+
+#RACES = {
+#    "Hill Dwarf":,
+#    "Montain Dwarf":,
+#    "High Elf":,
+#    "Wood Elf":,
+#    "Drow Elf":,
+#    "Lightfoot Halfling":,
+#    "Stout Halfling":,
+#    "Human":,
+#    "Dragonborn":,
+#    "Forest Gnome":,
+#    "Rock Gnome":,
+#    "Half-Elf":,
+#    "Half-Orc":,
+#    "Tiefling":
+#}
+
+class models:    
+    def __init__(self):
+        pass
--- races.py
+++ races.py
@@ -1,47 +1,14 @@
+import models
+
 class Race:
-    HUMAN_RACE_ABILITY_BONUS = {
-        "Strength":     1,
-        "Dexterity":    1,
-        "Constitution": 1,
-        "Intelligence": 1,
-        "Wisdom":       1,
-        "Charisma":     1
-    }
-
-    DRAGONBORN_RACE_ABILITY_BONUS = {
-        "Strength":     2,
-        "Charisma":     1
-    }
-
-    GNOME_FOREST_SUBRACE_ABILITY_BONUS = {
-        "Dexterity": 1,
-        "Intelligence": 2
-    }
-
-    GNOME_ROCK_SUBRACE_ABILITY_BONUS = {
-        "Constitution": 1,
-        "Intelligence": 2
-    }
-
-    HALF_ELF_RACE_ABILITY_BONUS = {
-        "Charisma":     2
-    }
-
-    HALF_ORC_RACE_ABILITY_BONUS = {
-        "Strength":     2,
-        "Constitution": 1
-    }
-
-    TIEFLING_RACE_ABILITY_BONUS = {
-        "Intelligence": 1,
-        "Charisma":     2
-    }
-
     def __init__(self):
         self.abilities_bonus = {}
         self.skills_bonus = {}
         self.proficiency = []
         self.language = ["Common"]
+        self.resistance = []
+        self.advantage = []
+        self.imunity = []
     
     def skill_check(self,skill):
         if self.skills_bonus.has_key(skill):
@@ -65,78 +32,161 @@ class Race:
         value = self.skills_bonus[skill]["bonus"]
         return value
 
+    def level_up(self):
+        pass
+
+    def race_attack(self):
+        pass
+
+    def race_proficiency(self):
+        pass
+
+    def race_language(self):
+        pass
+
 class Dwarf(Race):
     def __init__(self,subrace):
         super().__init__()
-        self.proficiency += self.DWARF_PROFICIENCY
-        self.language += self.DWARF_LANGUAGE
+        self.size =                 "Medium"
+        self.speed =                25
+        self.vision = {
+            "Bright light":         60,
+            "Dim light":            60,
+            "Darkness":             60
+        }
+        self.advantage =            ["Poison"]
+        self.resistance =           ["Poison"]
+        self.proficiency +=         models.DWARF_PROFICIENCY
 
         if subrace == "Hill Dwarf":
-            self.abilities_bonus = self.DWARF_HILL_SUBRACE_ABILITY_BONUS
+            self.abilities_bonus =  models.DWARF_HILL_SUBRACE_ABILITY_BONUS
         elif subrace == "Montain Dwarf":
-            self.abilities_bonus = self.DWARF_MONTAIN_SUBRACE_ABILITY_BONUS
-            self.proficiency += 
+            self.proficiency +=     models.DWARF_MONTAIN_SUBRACE_PROFICIENCY
+            self.abilities_bonus =  models.DWARF_MONTAIN_SUBRACE_ABILITY_BONUS
+        
+        self.language +=            models.DWARF_LANGUAGE
+
+        def level_up(self,character):
+            if character.sheet["race"] == "Hill Dwarf":
+                character.sheet["hp"] += 1
+            else:
+                pass
 
 class Elf(Race):
     def __init__(self,subrace):
         super().__init__()
-        self.proficiency += ["Perception"]
-        self.language += ["Elvish"]
+        self.proficiency +=         models.ELF_PROFICIENCY
+        self.language +=            models.ELF_LANGUAGE
         
         if subrace == "High Elf":
-            self.abilities_bonus = self.ELF_HIGH_SUBRACE_ABILITY_BONUS
-            self.proficiency += ["Longsword","Shortsword","Shortbow","Longbow"]
+            self.proficiency +=     models.ELF_HIGH_SUBRACE_PROFICIENCY
+            self.abilities_bonus =  models.ELF_HIGH_SUBRACE_ABILITY_BONUS
         elif subrace == "Wood Elf":
-            self.abilities_bonus = self.ELF_WOOD_SUBRACE_ABILITY_BONUS
-            self.proficiency += ["Longsword","Shortsword","Shortbow","Longbow"]
+            self.proficiency +=     models.ELF_WOOD_SUBRACE_PROFICIENCY
+            self.abilities_bonus =  models.ELF_WOOD_SUBRACE_ABILITY_BONUS
         elif subrace == "Drow Elf":
-            self.abilities_bonus = self.ELF_DROW_SUBRACE_ABILITY_BONUS
-            self.proficiency += ["Rapiers","Shortswords","Hand crossbows"]
+            self.proficiency +=     models.ELF_DROW_SUBRACE_PROFICIENCY
+            self.abilities_bonus =  models.ELF_DROW_SUBRACE_ABILITY_BONUS
 
 
 class Halfling(Race):
     def __init__(self,subrace):
         super().__init__()
         self.proficiency += []
-        self.language += ["Halfling"]
+        self.size = "Small"
+        self.advantage += ["Frightened"]
 
         if subrace == "Lightfoot Halfling":
-            self.abilities_bonus = self.HALFLING_LIGHTFOOT_SUBRACE_ABILITY_BONUS
+            self.abilities_bonus =  models.HALFLING_LIGHTFOOT_SUBRACE_ABILITY_BONUS
         elif subrace == "Stout Halfling":
-            self.abilities_bonus = self.HALFLING_STOUT_SUBRACE_ABILITY_BONUS
+            self.abilities_bonus =  models.HALFLING_STOUT_SUBRACE_ABILITY_BONUS
+            self.advantage +=       ["Poison"]
+            self.resistance +=      ["Poison"]
+
+        self.language +=            models.HALFLING_LANGUAGE
 
 class Human(Race):
     def __init__(self):
         super().__init__()
-        self.abilities_bonus = self.HUMAN_RACE_ABILITY_BONUS
+        self.abilities_bonus =      models.HUMAN_RACE_ABILITY_BONUS
+        self.size =                 "Medium"
+        self.speed =                30
+
+    def race_language(self,choice):
+        self.language +=            [choice]
 
 class Dragonborn(Race):
-    def __init__(self,subrace):
-        self.language += ["Draconic"]
+    def __init__(self,color):
+        self.language +=            models.DRAGONBORN_LANGUAGE
         super().__init__()
-        if subrace == "":
-            self.abilities_bonus = self.GNOME_FOREST_SUBRACE_ABILITY_BONUS
-        elif subrace == "":
-            self.abilities_bonus = self.GNOME_ROCK_SUBRACE_ABILITY_BONUS
-        self.abilities_bonus = self.DRAGONBORN_RACE_ABILITY_BONUS
+        self.abilities_bonus =      models.DRAGONBORN_RACE_ABILITY_BONUS
 
 class Gnome(Race):
-    def __init__(self):
-        self.language += ["Gnomish"]
+    def __init__(self,subrace):
+        self.language +=            models.GNOME_LANGUAGE
         super().__init__()
-        self.abilities_bonus = self.GNOME_ROCK_SUBRACE_ABILITY_BONUS
+        if subrace == "Forest Gnome":
+            self.abilities_bonus =  models.GNOME_FOREST_SUBRACE_ABILITY_BONUS
+        elif subrace == "Rock Gnome":
+            self.abilities_bonus =  models.GNOME_ROCK_SUBRACE_ABILITY_BONUS
 
 class HalfElf(Race):
     def __init__(self):
         super().__init__()
-        self.abilities_bonus = self.HALF_ELF_RACE_ABILITY_BONUS
+        self.abilities_bonus =      models.HALF_ELF_RACE_ABILITY_BONUS
+        self.size =                 "Medium"
+        self.speed =                30
+        self.vision = {
+            "Bright light":         60,
+            "Dim light":            60,
+            "Darkness":             60
+        }
+        self.advantage =            ["Charmed"]
+        self.imunity +=             [["Sleep","Magic"]]
+    
+    def race_proficiency(self,choice):
+        self.proficiency +=         [choice]
+
+    def race_language(self,choice):
+        self.language +=            [choice]
 
 class HalfOrc(Race):
     def __init__(self):
         super().__init__()
-        self.abilities_bonus = self.HALF_ORC_RACE_ABILITY_BONUS
+        self.abilities_bonus =      models.HALF_ORC_RACE_ABILITY_BONUS
+        self.size =                 "Medium"
+        self.speed =                30
+        self.vision = {
+            "Bright light":         60,
+            "Dim light":            60,
+            "Darkness":             60
+        }
+        self.proficiency +=         ["Intimidation"]
+        self.language +=            ["Orc"]
+
+    def race_attack(self,roll):
+        bonus_damage = 0
+        if roll == 20:
+            pass #TODO: receive roll, use weapon dice, return bonus damage
+        return bonus_damage
 
 class Tiefling(Race):
     def __init__(self):
         super().__init__()
-        self.abilities_bonus = self.TIEFLING_RACE_ABILITY_BONUS
+        self.abilities_bonus =      models.TIEFLING_RACE_ABILITY_BONUS
+        self.size =                 "Medium"
+        self.speed =                30
+        self.vision = {
+            "Bright light":         60,
+            "Dim light":            60,
+            "Darkness":             60
+        }
+        self.resistance +=          ["Fire"]
+        self.cantrip =              ["Thaummaturgy"]
+        self.language +=            ["Infernal"]
+
+    def level_up(self,character):
+        if character.sheet["level"] ==      3:
+            character.sheet["spells"] += ["Hellish rebuke"]
+        elif character.sheet["level"] ==    5:
+            character.sheet["spells"] += ["Darkness"]
